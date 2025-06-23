c2a1691acac7bbdb6febccec2942a7fef2e42879
Methode TWIZEYIMANA <tmmethode1@gmail.com>
2025-06-23 21:45:20 +0200

    new

--- website main/FURAHA/bload_donor/bload_donor/urls.py
+++
@@ -1,9 +0,0 @@
-# bload_donor/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('bload_donor_app.urls')),
-]
